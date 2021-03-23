cf3e27ca5293524166ff526c0391b94d2b7bd22f
vladlol001 <yakavitskiy001@mail.ru>
2021-03-23 09:58:41 +0300

    firsr commit

---
+++ js/main.js
@@ -0,0 +1,51 @@
+const mySwiper = new Swiper('.swiper-container', {
+	loop: true,
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.slider-button-next',
+		prevEl: '.slider-button-prev',
+	},
+});
+
+// Cart
+
+const buttonCart = document.querySelector('.button-cart');
+const modalCart = document.querySelector('#modal-cart');
+const modalClose = document.querySelector('.modal-close');
+
+
+const openModal = () => {
+	modalCart.classList.add('show');
+};
+
+const closeModal = () => {
+		modalCart.classList.remove('show');
+};
+
+const closeModalx2 = (e) => {
+	if(e.target.id === 'modal-cart')
+		modalCart.classList.remove('show');
+};
+
+buttonCart.addEventListener('click', openModal);
+modalCart.addEventListener('click', closeModalx2);
+modalClose.addEventListener('click', closeModal);
+
+// Scroll smooth
+
+const scrollLinks = document.querySelectorAll('a.scroll-link');
+
+{
+	for(let i = 0; i < scrollLinks.length; i++){
+		scrollLinks[i].addEventListener('click', (e) => {
+			e.preventDefault();
+			const id = scrollLinks[i].getAttribute('href');
+			document.querySelector(id).scrollIntoView({
+				behavior: "smooth",
+				block: 'start'
+			})
+		})
+	}
+}
+
