22e7aa9964ee7007c3f85311429c81afd0a97f4e
vladlol001 <yakavitskiy001@mail.ru>
2021-03-24 12:27:39 +0300

    second day

--- js/main.js
+++ js/main.js
@@ -37,10 +37,10 @@ modalClose.addEventListener('click', closeModal);
 const scrollLinks = document.querySelectorAll('a.scroll-link');
 
 {
-	for(let i = 0; i < scrollLinks.length; i++){
-		scrollLinks[i].addEventListener('click', (e) => {
+	for(scrollLink of scrollLinks){
+		scrollLink.addEventListener('click', (e) => {
 			e.preventDefault();
-			const id = scrollLinks[i].getAttribute('href');
+			const id = scrollLink.getAttribute('href');
 			document.querySelector(id).scrollIntoView({
 				behavior: "smooth",
 				block: 'start'
@@ -49,3 +49,91 @@ const scrollLinks = document.querySelectorAll('a.scroll-link');
 	}
 }
 
+// Goods
+
+const more = document.querySelector('.more');
+const navigationLink= document.querySelectorAll('.navigation-link');
+const longGoodsList= document.querySelector('.long-goods-list');
+
+const getGoods = async function(){
+	const result = await fetch('../db/db.json');
+	if(!result.ok){
+		throw "Error" + result.status;
+	} return await result.json();
+}
+
+const createCard = ({ description, price, label, img, name }) => {
+	const card = document.createElement('div');
+	card.className = 'col-lg-3 col-sm-6';
+
+	card.innerHTML = `
+		<div class="goods-card">
+			${label ? `<span class="label">${label}</span>` : ''}
+			<img src="db/${img}" alt="${name}" class="goods-image">
+			<h3 class="goods-title">${name}</h3>
+			<p class="goods-description">${description}</p>
+			<button class="button goods-card-btn add-to-cart" data-id="007">
+				<span class="button-price">$${price}</span>
+			</button>
+		</div>`
+		return card;
+};
+
+const renderCards = (data) => {
+	longGoodsList.innerHTML = '';
+	const cards = data.map(createCard);
+	longGoodsList.append(...cards)
+
+	document.body.classList.add('show-goods')
+};
+
+// Функция плавной прокрутки 
+const smoothScrolling = () => {
+	window.scrollTo({
+		top: 0,
+		behavior: "smooth",
+	})
+}
+
+more.addEventListener('click', (e) => {
+	e.preventDefault();
+	getGoods().then(renderCards);
+	// Плавная прокрутка вверх
+	smoothScrolling();
+})
+
+
+
+const filterCards = (field,value) =>{
+	getGoods().then((data) => {
+		// Добавление на All
+		if(value === 'All') 
+			return data.map((good) => good);
+		return data.filter((good) => good[field] === value);
+	})
+	.then(renderCards);
+}
+
+navigationLink.forEach((link) => 
+	link.addEventListener('click', (e) => {
+		e.preventDefault();
+		const field = link.dataset.field;
+		console.log(field);
+		let value = link.textContent;
+		console.log(value);
+		filterCards(field, value);
+}))
+
+// Добавление на View all
+
+const viewAll = document.querySelectorAll('.special-offers .button');
+
+viewAll.forEach((el, index) => {
+	el.addEventListener('click', () => {
+		if(el.className === 'button'){
+			if(index === 0) filterCards('category', 'Accessories');
+			if(index === 1) filterCards('category', 'Clothing');
+			smoothScrolling();
+		}
+	})
+})
