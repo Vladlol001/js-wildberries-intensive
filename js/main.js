a2ddaba312bb66548eb16cb0b087b01965bc8a07
vladlol001 <yakavitskiy001@mail.ru>
2021-03-25 23:35:11 +0300

    3 lesson

--- js/main.js
+++ js/main.js
@@ -13,10 +13,125 @@ const mySwiper = new Swiper('.swiper-container', {
 const buttonCart = document.querySelector('.button-cart');
 const modalCart = document.querySelector('#modal-cart');
 const modalClose = document.querySelector('.modal-close');
+const cardTableGoods = document.querySelector('.cart-table__goods');
+const cardTableTotal = document.querySelector('.card-table__total');
+const bucket = document.querySelector('.cart-count');
+const deleteAll = document.querySelector('.my-own-btn button');
 
+const getGoods = async function(){
+	const result = await fetch('../db/db.json');
+	if(!result.ok){
+		throw "Error" + result.status;
+	} return await result.json();
+}
+
+const cart = {
+	cartGoods: [
+		{
+			id: '100',
+			price: 100,
+			name: 'something',
+			count: 2,
+		}
+	],
+	renderCart(){
+		cardTableGoods.textContent = '';
+		this.cartGoods.forEach(({id,name,price,count}) => {
+			const trGood = document.createElement('tr');
+			trGood.className = 'cart-item';
+			trGood.dataset.id = id;
+
+			trGood.innerHTML = `
+					<td>${name}</td>
+					<td>${price}$</td>
+					<td><button class="cart-btn-minus" data-id="${id}">-</button></td>
+					<td>${count}</td>
+					<td><button class="cart-btn-plus" data-id="${id}">+</button></td>
+					<td>${price * count}$</td>
+					<td><button class="cart-btn-delete" data-id="${id}">x</button></td>
+			`;
+			cardTableGoods.append(trGood);
+		}) 
+		const totalPrice = this.cartGoods.reduce((sum, el) => {
+			return sum + el.price * el.count;
+		}, 0);
+		cardTableTotal.textContent = `${totalPrice}$`;
+	},
+	deleteGood(id){
+		this.cartGoods = this.cartGoods.filter(el => id != el.id);
+		this.renderCart();
+		bucket.textContent = this.cartGoods.length;
+	},
+	minusGood(id){
+		for( const item of this.cartGoods){
+			if(item.id == id ){
+				if(item.count <= 1){
+					this.deleteGood(id)
+				}else{
+					item.count--;
+					break;
+				}
+			}
+		}
+		this.renderCart();
+	},
+	plusGood(id){
+		for( const item of this.cartGoods){
+			if(item.id == id){
+				item.count++;
+				break;
+			}
+		}
+		this.renderCart();
+	},
+	addCartGoods(id){
+		const goodItem = this.cartGoods.find(item => item.id === id);
+		if(goodItem){
+			this.plusGood(id);
+		}else{
+			getGoods()
+				.then(data => data.find(item => item.id == id))
+				.then(({id,name,price}) => {
+					this.cartGoods.push({
+						id,
+						name,
+						price,
+						count: 1,
+					});
+					bucket.textContent = this.cartGoods.length;
+				})
+		}
+	},
+	deleteAllItems(){
+		this.cartGoods.forEach(el => this.deleteGood(el.id));
+	}
+}
+
+body.addEventListener('click', e => {
+	const addToCart = e.target.closest('.add-to-cart');
+	if(addToCart){
+		cart.addCartGoods(addToCart.dataset.id)
+	}
+})
+
+cardTableGoods.addEventListener('click', e => {
+	const target = e.target;
+	if(target.classList.contains('cart-btn-delete')){
+		cart.deleteGood(target.dataset.id);
+	} 
+	if(target.classList.contains('cart-btn-minus')){
+		cart.minusGood(target.dataset.id);
+	} 
+	if(target.classList.contains('cart-btn-plus')){
+		cart.plusGood(target.dataset.id);
+	} 
+})
+
+deleteAll.addEventListener('click', () => cart.deleteAllItems())
 
 const openModal = () => {
 	modalCart.classList.add('show');
+	cart.renderCart();
 };
 
 const closeModal = () => {
@@ -55,14 +170,7 @@ const more = document.querySelector('.more');
 const navigationLink= document.querySelectorAll('.navigation-link');
 const longGoodsList= document.querySelector('.long-goods-list');
 
-const getGoods = async function(){
-	const result = await fetch('../db/db.json');
-	if(!result.ok){
-		throw "Error" + result.status;
-	} return await result.json();
-}
-
-const createCard = ({ description, price, label, img, name }) => {
+const createCard = ({ id, description, price, label, img, name }) => {
 	const card = document.createElement('div');
 	card.className = 'col-lg-3 col-sm-6';
 
@@ -72,7 +180,7 @@ const createCard = ({ description, price, label, img, name }) => {
 			<img src="db/${img}" alt="${name}" class="goods-image">
 			<h3 class="goods-title">${name}</h3>
 			<p class="goods-description">${description}</p>
-			<button class="button goods-card-btn add-to-cart" data-id="007">
+			<button class="button goods-card-btn add-to-cart" data-id="${id}">
 				<span class="button-price">$${price}</span>
 			</button>
 		</div>`
@@ -118,9 +226,7 @@ navigationLink.forEach((link) =>
 	link.addEventListener('click', (e) => {
 		e.preventDefault();
 		const field = link.dataset.field;
-		console.log(field);
 		let value = link.textContent;
-		console.log(value);
 		filterCards(field, value);
 }))
 
